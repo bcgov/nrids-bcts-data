54c9efd6175e385797907cc59aa8ae4f81cd234c
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-04-24 15:40:07 -0700

    Added sql files for raods transferred in and out

---
+++ Reports/Roads Transferred Out/qRoadTransferredOut.sql
@@ -0,0 +1,58 @@
+SELECT DISTINCT
+    case
+        when
+            TSO_CODE in ('TBA', 'TPL', 'TPG', 'TSK', 'TSN', 'TCC', 'TKA', 'TKO', 'TOC')
+        then
+            'Interior'
+        when
+            TSO_CODE in ('TCH', 'TST', 'TSG')
+        then
+            'Coast'
+        end as BUSINESS_AREA_REGION_CATEGORY,
+    case
+        when
+            TSO_CODE in ('TBA', 'TPL', 'TPG', 'TSK', 'TSN')
+        then
+            'North Interior'
+        when
+            TSO_CODE in ('TCC', 'TKA', 'TKO', 'TOC')
+        then
+            'South Interior'
+        when
+            TSO_CODE in ('TCH', 'TST', 'TSG')
+        then
+            'Coast'
+        end as BUSINESS_AREA_REGION,
+    decode(
+        DIVI_DIVISION_NAME,
+        'Seaward',
+        'Seaward-Tlasta',
+        DIVI_DIVISION_NAME
+    ) || ' (' || TSO_CODE || ')' AS BUSINESS_AREA,
+    TSO_CODE as BUSINESS_AREA_CODE,
+    ROAD_SEQ_NBR,
+    URI,
+    ROAD_ROAD_NAME,
+    RCLS_ACCOUNTING_TYPE,
+    POC,
+    POT,
+    ((POT-POC)/1000) AS Length,
+    Trunc(DEAC_END_DATE) AS TRANSFER_DATE,
+    DEAC_METHOD_TYPE,
+    DEAC_LEVEL_TYPE,
+    RDST_STEWARD_NAME
+FROM
+    FORESTVIEW.V_ROAD_GAP_ANALYSIS
+WHERE
+    upper(DEAC_METHOD_TYPE) = 'TRANSFER OUT'
+    AND URI Is Not NULL
+    AND RCLS_ACCOUNTING_TYPE In ('1 Sale = 5 yrs', 'S.Term = 10 yrs', 'Perm = 40 yrs')
+    AND DEAC_END_DATE
+        BETWEEN To_Date('2024-04-01', 'YYYY-MM-DD')  /* Date: beginning of reporting period (usually beginning of current fiscal) */
+        AND To_Date('2025-03-31', 'YYYY-MM-DD')  /* Date: end of reporting period */
+ORDER BY
+    BUSINESS_AREA_REGION_CATEGORY desc,
+    BUSINESS_AREA_REGION,
+    BUSINESS_AREA,
+    ROAD_ROAD_NAME
+;
