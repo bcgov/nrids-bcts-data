3a8a31b7d73ee290d6dd3316615336a9225b7e86
Munthikodu <sreejith.munthikodu@gov.bc.ca>
2025-06-19 16:28:41 -0700

    BCTS Performance Report dashboard milestone 1

--- Reports/BCTS_performance_report/Original_queries/qLicenceIssued_detail.sql
+++ Reports/BCTS_performance_report/Original_queries/qLicenceIssued_detail.sql
@@ -1,4 +1,12 @@
 /* Change from Auction date to bidder_COUNT AS [# of Bidders; qlicenceissuedadvertised_main to qLicenceIssued_detail_main BD 2025-05-14*/
-SELECT business_area AS [Business Area], Licence, iif(Issued_Licence_BCTS_Category_Code in ('2', '4'), 'Value Added', '-') AS [Value Added], iif(Total_Volume_Salvage_All_Fire_Year_LRM > 0, 'Fire salvage', '-') AS [Includes Fire Salvage], bidder_COUNT AS [# of Bidders], Issued_Licence_Legal_Effective_Date AS Issued, Format(Issued_licence_volume, "#,###") AS [Volume (cubic metres)], Format(Issued_licence_maximum_value, "$#,###") AS [Max Value], Issued_licence_client_name AS Client
+SELECT business_area AS [Business Area],
+ Licence, 
+ iif(Issued_Licence_BCTS_Category_Code in ('2', '4'), 'Value Added', '-') AS [Value Added], 
+ iif(Total_Volume_Salvage_All_Fire_Year_LRM > 0, 'Fire salvage', '-') AS [Includes Fire Salvage],
+  bidder_COUNT AS [# of Bidders], 
+  Issued_Licence_Legal_Effective_Date AS Issued, 
+  Format(Issued_licence_volume, "#,###") AS [Volume (cubic metres)],
+   Format(Issued_licence_maximum_value, "$#,###") AS [Max Value],
+    Issued_licence_client_name AS Client
 FROM qLicenceIssuedAdvertised_main
 WHERE Issued_in_report_period = 'Y' and Issued_Licence_Legal_Effective_Date     between #05/16/2025#     and #05/31/2025#;
--- Reports/BCTS_performance_report/Original_queries/qNotAwarded.sql
+++ Reports/BCTS_performance_report/Original_queries/qNotAwarded.sql
@@ -1,3 +1,36 @@
-SELECT business_area AS [Business Area], Licence, iif(Last_Auction_BCTS_Category_Code in ('2', '4'), 'Value Added', '-') AS [Value Added], iif(Total_Volume_Salvage_All_Fire_Year_LRM > 0, 'Fire salvage', '-') AS [Includes Fire Salvage], Last_Auction_Date AS Auction, Format(Last_Auction_No_Sale_Volume, "#,###") AS [Volume (cubic metres)], Last_Auction_No_Sale_Rationale AS [No Sale Rationale]
+SELECT business_area AS [Business Area], 
+Licence, 
+iif(Last_Auction_BCTS_Category_Code in ('2', '4'), 'Value Added', '-') AS [Value Added], 
+iif(Total_Volume_Salvage_All_Fire_Year_LRM > 0, 'Fire salvage', '-') AS [Includes Fire Salvage], 
+Last_Auction_Date AS Auction, 
+Format(Last_Auction_No_Sale_Volume, "#,###") AS [Volume (cubic metres)], 
+Last_Auction_No_Sale_Rationale AS [No Sale Rationale]
 FROM qLicenceIssuedAdvertised_main
-WHERE Advertised_in_Report_Period = 'Y'     and Last_Auction_No_Sale_Rationale is not null     and Last_Auction_Date         between #05/16/2025#         and #05/31/2025#;
+WHERE Advertised_in_Report_Period = 'Y'     
+and Last_Auction_No_Sale_Rationale is not null     
+and Last_Auction_Date         between #05/16/2025#         and #05/31/2025#;
+
+
+
+-- Postgres
+
+SELECT 
+    business_area AS "Business Area", 
+    Licence, 
+    CASE 
+        WHEN Last_Auction_BCTS_Category_Code IN ('2', '4') THEN 'Value Added' 
+        ELSE '-' 
+    END AS "Value Added", 
+    CASE 
+        WHEN Total_Volume_Salvage_All_Fire_Year_LRM > 0 THEN 'Fire salvage' 
+        ELSE '-' 
+    END AS "Includes Fire Salvage", 
+    Last_Auction_Date AS Auction, 
+    TO_CHAR(Last_Auction_No_Sale_Volume, 'FM999,999,999') AS "Volume (cubic metres)", 
+    Last_Auction_No_Sale_Rationale AS "No Sale Rationale"
+FROM 
+    qLicenceIssuedAdvertised_main
+WHERE 
+    Advertised_in_Report_Period = 'Y'     
+    AND Last_Auction_No_Sale_Rationale IS NOT NULL     
+    AND Last_Auction_Date BETWEEN DATE '2025-05-16' AND DATE '2025-05-31';
